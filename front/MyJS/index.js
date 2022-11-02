16a738c3d0a80418d0da986d193b6d81be8ba34e
Kevin Beghelli <kevin.beghelli@gmail.com>
2022-11-02 19:10:46 +0100

    test

--- front/MyJS/index.js
+++ front/MyJS/index.js
@@ -15,9 +15,9 @@ function dynamicProducts() {
 
         /*  Il reste à insérer le lien vers la page du produit : <a href="./product.html?id=42">
         * donc surement avec ce lien : ./product.html?id=${product._id}
-        *
-        insertHtml.setAttributes("href", "./product.html?id=${product._id}")
         */
+        insertHtml.setAttributes("href", `./product.html?id=${product._id}`)
+        
 
         insertHtml.innerHTML = `<article>
       <img src="${product.imageUrl}" alt="${product.altTxt}">
