484dd10b42385cf4ca6234a55731db2eefe7f439
Kevin Beghelli <kevin.beghelli@gmail.com>
2022-11-06 19:10:03 +0100

    1 heure de perdue à cause d'un "s" à attribute

--- front/MyJS/index.js
+++ front/MyJS/index.js
@@ -14,10 +14,9 @@ function dynamicProducts() {
         let insertHtml = document.createElement("a");
 
         /*  Il reste à insérer le lien vers la page du produit : <a href="./product.html?id=42">
-        * donc surement avec ce lien : ./product.html?id=${product._id}
-        
-        insertHtml.setAttributes("href", `./product.html?id=${product._id}`)
-        */
+        * donc surement avec ce lien : ./product.html?id=${product._id} */
+        insertHtml.setAttribute("href", `./product.html?id=${product._id}`)
+
 
         insertHtml.innerHTML = `<article>
       <img src="${product.imageUrl}" alt="${product.altTxt}">
