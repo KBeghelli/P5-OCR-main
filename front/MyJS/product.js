7b620876a10144a635694f50efe8fc715fad7456
Kevin Beghelli <kevin.beghelli@gmail.com>
2022-11-07 22:19:18 +0100

    urlsearchparams pour page produit

--- front/MyJS/product.js
+++ front/MyJS/product.js
@@ -1,7 +1,10 @@
 /*
 Même système que index.js ? 
 fetch("http://localhost:3000/api/products/" + id)
-en insérant le nom, description, prix de chaque du produit correspondant à l'id
+en insérant le nom, description, prix de chaque du produit correspondant à l'id */
+
+let params = new URLSearchParams(document.location.search);
+let idProduct = params.get("id");
 
 
 
