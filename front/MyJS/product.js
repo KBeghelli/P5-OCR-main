8f4b24b67ff0b551d0e8a4c4f94b188e7530c7f2
Kevin Beghelli <kevin.beghelli@gmail.com>
2022-11-08 09:16:52 +0100

    doesn't work, test to see what's the prob

--- front/MyJS/product.js
+++ front/MyJS/product.js
@@ -1,7 +1,7 @@
 /*
 Même système que index.js ? 
 fetch("http://localhost:3000/api/products/" + id)
-en insérant le nom, description, prix de chaque du produit correspondant à l'id */
+en insérant le nom, description, prix de chaque du produit correspondant à l'id 
 
 let params = new URLSearchParams(document.location.search);
 import { urlProductsData } from "index.js";
@@ -14,3 +14,4 @@ fetch("urlProductsData" + idProduct).then((response) => {
     }
   })
 }
+*/
