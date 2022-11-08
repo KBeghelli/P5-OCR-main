8427ac5db6ead9d0c23e91ac811788a448b529c5
Kevin Beghelli <kevin.beghelli@gmail.com>
2022-11-08 16:50:57 +0100

    fazfazfaf

--- front/MyJS/product.js
+++ front/MyJS/product.js
@@ -1,3 +1,47 @@
-const test = document.getElementById('title');
+/*const test = document.getElementById('title');
 test.textContent = "Ceci est un test";
-console.log(test)
+
+
+let params = (new URL(document.location)).searchParams;
+let idProduct = params.get("id");*/
+
+let params = new URL(window.location.href).searchParams;
+// j'indique que la nouvelle url sera ajoutée d'un id :
+let newID = params.get('id');
+
+//---------J'APPELLE DE NOUVEAU L'API AVEC L'ID DU CANAPE CHOISI---------
+
+// je crée les variables dont j'ai besoin pour manipuler cette page :
+const image = document.getElementsByClassName('item__img');
+const title = document.getElementById('title');
+const price = document.getElementById('price');
+const description = document.getElementById('description');
+const colors = document.getElementById('colors');
+
+let imageURL = "";
+let imageAlt = "";
+
+// je crée la bonne URL pour chaque produit choisi en ajoutant newID
+fetch("http://localhost:3000/api/products/" + newID)
+  .then(res => res.json())
+  .then(data => {
+    // je modifie le contenu de chaque variable avec les bonnes données :
+    image[0].innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
+    imageURL = data.imageUrl;
+    imageAlt = data.altTxt;
+    title.innerHTML = `<h1>${data.name}</h1>`;
+    price.innerText = `${data.price}`;
+    description.innerText = `${data.description}`;
+
+    // je configure le choix des couleurs 
+    for (number in data.colors) {
+      colors.options[colors.options.length] = new Option(
+        data.colors[number],
+        data.colors[number]
+      );
+    }
+  })
+    // j'ajoute un message au cas où le serveur ne répond pas
+  .catch(_error => {
+    alert('Oops ! Le serveur ne répond pas, suivez les instructions dans le READ.me.');
+  });
