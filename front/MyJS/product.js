e5fc40029c6e791db2e0c0eb6f54648c914881a2
Kevin Beghelli <kevin.beghelli@gmail.com>
2022-11-08 16:57:05 +0100

    test

--- front/MyJS/product.js
+++ front/MyJS/product.js
@@ -5,24 +5,17 @@ test.textContent = "Ceci est un test";*/
 let params = (new URL(document.location)).searchParams;
 let idProduct = params.get("id");
 
-const image = document.getElementsByClassName('item__img');
 const title = document.getElementById('title');
 const price = document.getElementById('price');
 const description = document.getElementById('description');
 const colors = document.getElementById('colors');
 
-let imageURL = "";
-let imageAlt = "";
 
 // je crée la bonne URL pour chaque produit choisi en ajoutant newID
 fetch("http://localhost:3000/api/products/" + idProduct)
   .then(res => res.json())
   .then(data => {
-    // je modifie le contenu de chaque variable avec les bonnes données :
-    image[0].innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
-    imageURL = data.imageUrl;
-    imageAlt = data.altTxt;
-    title.innerHTML = `<h1>${data.name}</h1>`;
+    title.innerHTML = `${data.name}`;
     price.innerText = `${data.price}`;
     description.innerText = `${data.description}`;
 
@@ -36,5 +29,5 @@ fetch("http://localhost:3000/api/products/" + idProduct)
   })
     // j'ajoute un message au cas où le serveur ne répond pas
   .catch(_error => {
-    alert('Oops ! Le serveur ne répond pas, suivez les instructions dans le READ.me.');
+    alert('BUG');
   });
