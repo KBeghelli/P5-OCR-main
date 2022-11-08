be2309670c84b0c57126c2a17c15347cb94ffd2f
Kevin Beghelli <kevin.beghelli@gmail.com>
2022-11-08 09:29:40 +0100

    test

--- front/MyJS/product.js
+++ front/MyJS/product.js
@@ -4,7 +4,7 @@ fetch("http://localhost:3000/api/products/" + id)
 en insérant le nom, description, prix de chaque du produit correspondant à l'id 
 
 let params = new URLSearchParams(document.location.search);
-import { urlProductsData } from "index.js";
+import {urlProductsData} from "index.js";
 let idProduct = params.get("id");
 
 function afficherProduit() {
