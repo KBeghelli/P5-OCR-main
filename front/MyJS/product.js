5ca2eaea13238efb06169e55f9d5e45aef2b2c5b
Kevin Beghelli <kevin.beghelli@gmail.com>
2022-11-13 14:07:22 +0100

    fix image

--- front/MyJS/product.js
+++ front/MyJS/product.js
@@ -10,12 +10,7 @@ const colors = document.getElementById('colors');
 fetch('http://localhost:3000/api/products/' + idProduct)
   .then(res => res.json())
   .then(data => {
-
-    // pour les images, il faut à la fois insérer le src et le alt
-    // image.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`
-    /*image[0].innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
-    imageURL = data.imageUrl;
-    imageAlt = data.altTxt;*/
+    
     let productImg = document.createElement("img");
     document.querySelector(".item__img").appendChild(productImg);
     productImg.src = data.imageUrl;
