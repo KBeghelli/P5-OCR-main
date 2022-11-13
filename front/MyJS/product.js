f2eb2e15448b4316a83825a9ba3d62e497201a45
Kevin Beghelli <kevin.beghelli@gmail.com>
2022-11-13 13:49:42 +0100

    image and color choice

--- front/MyJS/product.js
+++ front/MyJS/product.js
@@ -14,15 +14,16 @@ fetch('http://localhost:3000/api/products/' + idProduct)
 
 // pour les images, il faut à la fois insérer le src et le alt
 // image.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`
+    image.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`
     title.innerText = `${data.name}`;
     price.innerText = `${data.price}`;
     description.innerText = `${data.description}`;
  
- /*   for (let color of data.colors) {
+   for (let color of data.colors) {
         let productColors = document.createElement("test");
         document.querySelector("#colors").appendChild(productColors);
     }
-*/
+
   })
   .catch(_error => {
     alert('BUG');
