4f1fbda0ec617e0b213e472f8dae24bfb167e62a
Kevin Beghelli <kevin.beghelli@gmail.com>
2022-11-13 13:56:07 +0100

    test

--- front/MyJS/product.js
+++ front/MyJS/product.js
@@ -12,18 +12,18 @@ fetch('http://localhost:3000/api/products/' + idProduct)
   .then(res => res.json())
   .then(data => {
 
-// pour les images, il faut à la fois insérer le src et le alt
-// image.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`
-    image.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`
+    // pour les images, il faut à la fois insérer le src et le alt
+    // image.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`
+    image[0].innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
     imageURL = data.imageUrl;
     imageAlt = data.altTxt;
     title.innerText = `${data.name}`;
     price.innerText = `${data.price}`;
     description.innerText = `${data.description}`;
- 
-   for (let color of data.colors) {
-        let productColors = document.createElement("test");
-        document.querySelector("#colors").appendChild(productColors);
+
+    for (let color of data.colors) {
+      let productColors = document.createElement("test");
+      document.querySelector("#colors").appendChild(productColors);
     }
 
   })
