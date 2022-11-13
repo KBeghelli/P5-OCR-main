9e2e3b6a1a2b3972fc24d8939972d12c9255fca0
Kevin Beghelli <kevin.beghelli@gmail.com>
2022-11-13 14:19:39 +0100

    zfafazfaf

--- front/MyJS/product.js
+++ front/MyJS/product.js
@@ -12,20 +12,15 @@ const image = document.querySelector("article div.item__img");
 fetch('http://localhost:3000/api/products/' + idProduct)
   .then(res => res.json())
   .then(data => {
-
-    /*let productImg = document.createElement("img");
-    document.querySelector(".item__img").appendChild(productImg);
-    productImg.src = data.imageUrl;
-    productImg.alt = data.altTxt;*/
     image.innerHTML = `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
-
     title.innerText = `${data.name}`;
     price.innerText = `${data.price}`;
     description.innerText = `${data.description}`;
 
     for (let color of data.colors) {
-      let productColors = document.createElement("test");
-      document.querySelector("#colors").appendChild(productColors);
+      //let productColors = document.createElement("test");
+      //document.querySelector("#colors").appendChild(productColors);
+      colors.innerHTML += `<option value="${color}">${color}</option>`;
     }
 
   })
