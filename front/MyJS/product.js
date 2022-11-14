6e82fcf3f5c693262b081b23eccf562b0479915d
Kevin Beghelli <kevin.beghelli@gmail.com>
2022-11-14 11:33:25 +0100

    zdzdz

--- front/MyJS/product.js
+++ front/MyJS/product.js
@@ -27,3 +27,32 @@ fetch('http://localhost:3000/api/products/' + idProduct)
   .catch(_error => {
     alert('BUG');
   });
+
+/**********************************************************************************************************/
+
+/* FONCTION D'AJOUT AU PANIER
+* On sélectionne le bouton AddToCart, et on écoute ses évènements
+* Si les données entrées sont valides (quantité entre 1 et 100 et couleur sélectionné), 
+* alors on affiche un message "Produit ajouté au panier !" et on appelle une fonction d'ajout au panier,
+* sinon (si quantité ou couleur non sélectionné par exemple), afficher message d'erreur
+*/
+
+/*const idQuantity = document.getElementById('quantity');
+const idColors = document.getElementById('colors');
+
+const addToCart = document.getElementById('addToCart');
+
+addToCart.addEventListener("click", () => {
+if
+
+}) */
+
+/* Comme préciser dans le "guide des étapes clés" : 
+* "Nous devons récupérer un array contenant trois choses : l'id, la quantité et la couleur du produit", 
+* pour ensuite pouvoir ajouter cette array au panier
+* Je vais donc créer une nouvelle variable vide, prête à contenir cette array. Je vais immédiatement y ajouter
+* l'id du produit que nous connaissons déjà, et ensuite ajouter des EventListener sur la quantité et la couleur
+* pour connaitre la sélection de l'utilisateur */
+
+let chosenProduct = {};
+chosenProduct._id = idProduct;
